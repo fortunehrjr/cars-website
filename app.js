9432d87f8cc33dc0ef59c2e168c12f8e6d56e645
Fortune <83907042+fortunehrjr@users.noreply.github.com>
2023-05-06 12:03:03 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,194 @@
+let controller;
+let slideScene;
+let PageScene;
+let detailScene;
+
+function animateSlides() {
+  // initiate controller
+  controller = new ScrollMagic.Controller();
+  // select things
+  let sliders = document.querySelectorAll(".slide");
+  let nav = document.querySelector(".nav-header");
+  //loop over each slide
+  sliders.forEach((slide, index, slides) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+    //GSAP
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: "2" }, { scale: "1" }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=.8");
+    // CREATE SCENE
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addTo(controller);
+    // new animation
+    const pageTl = gsap.timeline();
+    let nextSlide = slide.length - 1 === index ? "end" : slide[index + 1];
+    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+    // create new scene
+    PageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
+      .addTo(controller);
+  });
+}
+
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector(".cursor-text");
+const burger = document.querySelector(".burger");
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  //ghgh
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    mouseText.innerHTML = "Tap";
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    // titleSwipe.style.transform = "translateY(0%)";
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseText.innerHTML = "";
+    // titleSwipe.style.transform = "translateY(100%)";
+    gsap.to(".title-swipe", 1, { y: "100%" });
+  }
+}
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45deg", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45deg", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+    gsap.to(mouse, 1, { border: " 2px solid black" });
+    mouse.style.zIndex = "1";
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+    gsap.to(mouse, 1, { border: " 2px solid white" });
+  }
+}
+
+// BARBA PAGE TRANSITIONS
+const logo = document.querySelector("#logo");
+barba.init({
+  views: [
+    {
+      namespace: "home",
+      beforeEnter() {
+        animateSlides();
+        logo.href = "./index.html";
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        PageScene.destroy();
+        controller.destroy();
+      },
+    },
+    {
+      namespace: "car",
+      beforeEnter() {
+        logo.href = "../index.html";
+        detailAnim();
+      },
+      beforLeave() {
+        controller.destroy();
+        detailScene.destroy();
+      },
+    },
+  ],
+  transitions: [
+    {
+      leave({ current, next }) {
+        let done = this.async();
+        // animation
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+        tl.fromTo(
+          ".swipe",
+          0.75,
+          { x: "-100%" },
+          { x: "0%", onComplete: done },
+          "-=0.5"
+        );
+      },
+      enter({ current, next }) {
+        // animation
+        // scroll up
+        window.scrollTo(0, 0);
+        let done = this.async();
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(
+          ".swipe",
+          0.75,
+          { x: "0%" },
+
+          { x: "100%", stagger: 0.25, onComplete: done }
+        );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
+        );
+      },
+    },
+  ],
+});
+
+function detailAnim() {
+  controller = new ScrollMagic.Controller();
+
+  const slides = document.querySelectorAll(".detail-slide");
+  slides.forEach((slide, index, slides) => {
+    const slideTl = gsap.timeline({ defaults: { duration: 1 } });
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    const nextImg = nextSlide.querySelector("img");
+    slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+    slideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, "-=1");
+    slideTl.fromTo(nextImg, { x: "-100%" }, { x: "0%" }, "-=1");
+    // create scene
+    detailScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(slideTl)
+      .addTo(controller);
+  });
+}
+
+//EVENT LISTENERS
+burger.addEventListener("click", navToggle);
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
